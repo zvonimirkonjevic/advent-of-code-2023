fc778e026ba47094c68c63cdcea87045d0621203
zvonimirkonjevic <zvonimir.konjevic@gmail.com>
2023-12-06 10:21:12 +0100

    implement solution to part 2 of day 3 problem

--- 3-gear-ratios/solution.py
+++ 3-gear-ratios/solution.py
@@ -19,7 +19,7 @@ def is_adjacent_to_symbol(row, col):
 with open('input.txt', 'r') as f:
     for line in f.readlines():
         inputs.append(list(line.strip()))
-total = 0
+total_parts = 0
 for i, row in enumerate(inputs):
     j = 0
     while j < len(row):
@@ -31,7 +31,33 @@ for i, row in enumerate(inputs):
             for col_index in range(init_j, j + 1):
                 visited.add((i, col_index))
                 if is_adjacent_to_symbol(i, col_index):
-                    total += int(number)
+                    total_parts += int(number)
                     break
         j += 1
-print(total)
+total_gear_ratios = 0
+for i,row in enumerate(inputs):
+    for j, char in enumerate(row):
+        if char == '*':
+            adjecent_numbers = []
+            for dx, dy in DIRECTIONS:
+                adjecent_row, adjecent_col = i + dx, j + dy
+                if 0 <= adjecent_row < len(inputs) and 0 <= adjecent_col < len(inputs[adjecent_row]):
+                    if inputs[adjecent_row][adjecent_col].isdigit():
+                        number_str = inputs[adjecent_row][adjecent_col]
+                        left_col = adjecent_col - 1
+                        while left_col >= 0 and inputs[adjecent_row][left_col].isdigit():
+                            number_str = inputs[adjecent_row][left_col] + number_str
+                            left_col -= 1
+
+                        right_col = adjecent_col + 1
+                        while right_col < len(inputs[adjecent_row]) and inputs[adjecent_row][right_col].isdigit():
+                            number_str = number_str + inputs[adjecent_row][right_col]
+                            right_col += 1
+                        part_number = int(number_str)
+                        if part_number not in adjecent_numbers:
+                            adjecent_numbers.append(part_number)
+            if len(adjecent_numbers) == 2:
+                total_gear_ratios += adjecent_numbers[0] * adjecent_numbers[1]
+
+print(total_parts)
+print(total_gear_ratios)
