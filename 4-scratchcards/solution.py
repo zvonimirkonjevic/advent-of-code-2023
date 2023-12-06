14aa10c1810771b234322622a33eacb53283f77f
zvonimirkonjevic <zvonimir.konjevic@gmail.com>
2023-12-06 13:08:03 +0100

    implement solution to part 2 of day 4 problem

--- 4-scratchcards/solution.py
+++ 4-scratchcards/solution.py
@@ -4,7 +4,8 @@ with open("input.txt", "r") as input:
     for line in input.readlines():
         inputs.append(re.sub(r'^.*: ', '', line).rstrip())
 total_points = 0
-for input in inputs:
+total_scratchcards = [1]*200
+for i,input in enumerate(inputs):
     winning_numbers, card_numbers = input.split('|')
     winning_numbers = winning_numbers.split(' ')
     card_numbers =  card_numbers.split(' ')
@@ -19,4 +20,7 @@ for input in inputs:
         if number in card_numbers:
             count += 1
     total_points += pow(2, count-1) if (count > 0) else 0
+    for j in range(count):
+        total_scratchcards[j + i + 1] = total_scratchcards[j + i + 1] + total_scratchcards[i]
 print(total_points)
+print(sum(total_scratchcards[:i + 1]))
